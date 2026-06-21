b03430a56a710e8b8f95fc6b98f4e72c4660d900
ismet1453 <ismetemir4040@gmail.com>
2026-06-21 21:06:55 +0300

    Fix TonConnect manifest with square icon and dynamic Vercel manifest

--- server.js
+++ server.js
@@ -4,14 +4,33 @@ const cors = require('cors');
 const path = require('path');
 const { connectDB } = require('./database');
 const apiRoutes = require('./routes/api');
+const campaignRoutes = require('./routes/campaign');
 const { startDepositWatcher } = require('./services/tonDepositWatcher');
 const { processDeposit } = require('./services/gameService');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/tonconnect-manifest.json', (req, res) => {
+    const proto = req.get('x-forwarded-proto') || req.protocol || 'https';
+    const host = req.get('x-forwarded-host') || req.get('host');
+    const origin = `${proto}://${host}`.replace(/\/$/, '');
+    res.set('Cache-Control', 'no-store');
+    res.set('Access-Control-Allow-Origin', '*');
+    res.json({
+        url: origin,
+        name: 'Gacha Cup',
+        iconUrl: `${origin}/icon.png`,
+        termsOfUseUrl: 'https://t.me/FootballGacha',
+        privacyPolicyUrl: 'https://t.me/FootballGacha'
+    });
+});
+
+app.use('/airdrop', express.static(path.join(__dirname, 'website')));
+app.use('/site', express.static(path.join(__dirname, 'public/site')));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/api/campaign', campaignRoutes);
 app.use('/api', apiRoutes);
 
 app.get('/health', (_req, res) => {
